121cd2774c1382414ef309d0ab6d3bbd8279291c
SalmanCorleone <salmansantino@gmail.com>
2018-11-26 07:17:02 +0600

    Nov 26

--- app/navigation/details.js
+++ app/navigation/details.js
@@ -1,32 +1,32 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Title from '../components/Title';
 
 export default class details extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-  render() {
-    const { navigation } = this.props;
-    const itemId = navigation.getParam("itemId", "NO-ID");
-    const item = navigation.getParam("item", "Not Assigned");
-    return (
-      <View style={styles.back}>
-        <Title />
-        <Text> ID: {itemId} </Text>
-        <Text> Task: {item.text} </Text>
-        <Text> Date: {item.date} </Text>
-        <Text> Type: {item.type} </Text>
-      </View>
-    );
-  }
+	render() {
+		const { navigation } = this.props;
+		const itemId = navigation.getParam('itemId', 'NO-ID');
+		const item = navigation.getParam('item', 'Not Assigned');
+		return (
+			<View style={styles.back}>
+				<Title />
+				<Text> ID: {itemId} </Text>
+				<Text> Task: {item.text} </Text>
+				<Text> Date: {item.date.toString()} </Text>
+				<Text> Type: {item.type} </Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  back: {
-    backgroundColor: "#2c3e50",
-    flex: 1
-  }
+	back: {
+		backgroundColor: '#2c3e50',
+		flex: 1
+	}
 });
